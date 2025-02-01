65bd9fe651dc800cc3dd8c25421c4113873a9556
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-02-01 11:41:28 -0600

    Hamburger funciona

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,2 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.getElementById('navlinks');
 
+hamburger.addEventListener('click', () => {
+    console.log('clicked');
+    console.log(navlinks);
+    
+    navlinks.classList.toggle('active');
+})
 
