2aa40d463ebec15b545d09932a84a8a84732052c
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-02-01 11:45:16 -0600

    FIX- CORRECTIONS

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,10 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navlinks = document.getElementById('navlinks');
 
-hamburger.addEventListener('click', () => {
-    console.log('clicked');
-    console.log(navlinks);
-    
+const year = document.querySelector('#currentyear');
+const lastModify = document.querySelector('#lastModified');
+
+const today = new Date();
+
+year.innerHTML = `${today.getFullYear()}`;
+lastModify.innerHTML = `${document.lastModified}`
+
+hamburger.addEventListener('click', () => {    
     navlinks.classList.toggle('active');
 })
 
