7adc2cda44ccc23083c6eb9c0219f19aaaf9ed3c
Yaelvc <dayaelvc@gmail.com>
2026-03-15 17:36:15 -0600

    Add - dinamics with js

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,7 +1,15 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").innerHTML = currentYear;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+
 
 hamButton.addEventListener('click', () => {
+    console.log('hola');
+    
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
